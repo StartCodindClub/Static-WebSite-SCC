31fac904747473fc74385697c4c77244c710450e
StartCodindClub <88532819+StartCodindClub@users.noreply.github.com>
2021-08-06 10:16:15 +0100

    Add files via upload

---
+++ Baiche Mohamed El Amine/README.md
@@ -0,0 +1,25 @@
+
+######      NTIC WEBSITE - AKA FIRST WEBSITE.
+
+
+######      "NTIC" Logo made by me.
+
+
+######      Illustrations were taken from /* https://storyset.com/  */
+
+
+######      Main colors used in the design are :  ***#292929, #273150, #ABB0FF, #666DF7, #192023, #D2D6DC, #FEFEFE.***
+
+
+######      Icons used were taken from /* https://remixicon.com/ -- https://www.flaticon.com/ */
+
+
+######     English font used : ***"Prompt" , sans serif.***
+
+
+######     Arabic font used : ***"Noor", "Noor Light", sans serif.***
+
+
+######     ***STILL WORKING ON THE WEBSITE***
+
+
---
+++ Baiche Mohamed El Amine/app.js
@@ -0,0 +1,129 @@
+/*==================== loader ====================*/
+
+
+/* ##### SHOW MENU / ALSO IN CSS #### */
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/* ##### Menu showing #### */
+/* Validate if constant exist */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* ##### Menu hiding #### */
+/* Validate if constant exist */
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/* ##### REMOVING MENU FOR MOBILE WHEN CLICKING #### */
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+        // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* ##### CHANGE NAV BG WHEN SCROLLING / ALSO IN CSS #### */
+function scrollHeader() {
+    const header = document.getElementById('header')
+        // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 100) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-fill'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-fill'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
+/* cursor */
